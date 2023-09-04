faeb638735349b619f0b7872c3d8e2b565509f66
bohdan <bohdanvelhan0@gmail.com>
2023-09-04 21:26:28 +0300

    new project

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import './style.css'
 import App from './App.vue'
-import {router} from "@/plugins/router";
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
