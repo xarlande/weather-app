8b231ae6700da7c26758dbd049eb9c89a69177db
xarlande <bohdanvelhan0@gmail.com>
2026-03-02 15:09:33 +0200

    add storybook

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -3,18 +3,23 @@ import { ofetch } from "ofetch";
 
 const APIkey = "83066a7babc02d04e934fe54a19e1ab9";
 
+const api = ofetch.create({
+  query: {
+    appid: APIkey,
+  },
+});
+
 export async function fetchGeolocation(
   query: string
 ): Promise<GeolocationItem[]> {
   if (!query) return [];
 
-  return await ofetch<GeolocationItem[]>(
+  return await api<GeolocationItem[]>(
     "https://api.openweathermap.org/geo/1.0/direct",
     {
       query: {
         q: query,
         limit: 5,
-        appid: APIkey,
       },
     }
   );
@@ -24,13 +29,12 @@ export async function fetchWeatherForecast(
   lat: number,
   lon: number
 ): Promise<WeatherResponse> {
-  return await ofetch<WeatherResponse>(
+  return await api<WeatherResponse>(
     "https://api.openweathermap.org/data/2.5/forecast",
     {
       query: {
         lat,
         lon,
-        appid: APIkey,
         units: "metric",
         lang: "ua",
       },
